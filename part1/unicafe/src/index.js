99a3f5c4563612346aa959a690658145ef01bbd0
Manu Thomas <manu.thomas@eformsolutions.com>
2021-01-27 15:30:56 +0530

    Exercise 1.8 Moved statistics to its own Statistics component.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+  const {good, neutral, bad} = props;
+  const all = (good + neutral + bad);
+  const average = ((good - bad) / all);
+  const goodPercentage = Math.round(((good / all) * 100) * 100) / 100;
+
+  return (
+    <div className="statistics">
+      <h4 className="heading">statistics</h4>
+      <br />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      {(!isNaN(average)) && <p>average {average}</p>}
+      {(!isNaN(goodPercentage)) && <p>positive {goodPercentage} %</p>}
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0);
-  const all = (good + neutral + bad);
-  const average = ((good - bad) / all); //Math.round(num * 100) / 100
-  const goodPercentage = Math.round(((good/all) * 100) * 100) / 100; 
 
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
@@ -24,14 +41,7 @@ const App = () => {
         <button className="bad" onClick={handleBadClick}>Bad</button>
       </div>
       <br />
-      <h4 className="statics">statistics</h4>
-      <br />
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      {(!isNaN(average)) && <p>average {average}</p>} 
-      {(!isNaN(goodPercentage)) && <p>positive {goodPercentage} %</p>}
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
