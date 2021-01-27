155bd7829ac31c3be5f958c661ddbc805f9a1c23
Manu Thomas <manu.thomas@eformsolutions.com>
2021-01-27 15:24:05 +0530

    Exercise 1.7 - unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,7 +5,10 @@ const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [bad, setBad] = useState(0);
+  const all = (good + neutral + bad);
+  const average = ((good - bad) / all); //Math.round(num * 100) / 100
+  const goodPercentage = Math.round(((good/all) * 100) * 100) / 100; 
 
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
@@ -26,6 +29,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      {(!isNaN(average)) && <p>average {average}</p>} 
+      {(!isNaN(goodPercentage)) && <p>positive {goodPercentage} %</p>}
     </div>
   );
 }
